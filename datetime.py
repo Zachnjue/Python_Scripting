b2518cf6c967f020cc9d390fe850cbf1ca1be663
Zachnjue <njuezachary@gmail.com>
2022-01-05 21:44:17 +0300

    added a datetime file

---
+++ datetime.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+#from pytz import timezone
+#import pytz     # in full - python timezone
+
+# work with the timezone of the host
+print(datetime.now())
+# work with another timezone

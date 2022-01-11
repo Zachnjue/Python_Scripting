531f90e77a7d47dd83fb4505b3763f78937ef8bf
Zacharia Njue <njuezachary@gmail.com>
2022-01-11 17:59:37 +0300

    added a subprocess_Module.py file

---
+++ subprocess_Module.py
@@ -0,0 +1,12 @@
+# This is used to execute Operating System commands
+import subprocess
+
+# For shell to be True, cmd is listed as a string while False is when the command is in List format
+cmd = 'ls -lrt'
+sp = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True )
+rt = sp.wait()
+out, err = sp.communicate()
+
+print(f'The return code is: {rt}')
+print(f'The output is: \n{out.splitlines()}') # use of .splitlines() leads the output to be a list
+print(f'The error is: \n{err.splitlines()}')

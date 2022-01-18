cd9735fd8a27d134554897e20fae9d5952e50d9f
Zacharia Njue <njuezachary@gmail.com>
2022-01-18 20:40:14 +0300

    added a line to improve the ouput

--- CSV/ReadCSV_file.py
+++ CSV/ReadCSV_file.py
@@ -3,7 +3,10 @@ req_file = '/Users/zachnjue/Documents/Python /Scripting/CSV/info.csv'
 
 file_open = open(req_file, 'r')
 data = csv.reader(file_open, delimiter=',')    # use the Reader method to read the csv file. also important to use the delimiter option to separate the values
+# Print the data as one list
+print(list(data)[0])
 
+# print data as each item of the list
 for each in data:
     print(each)
-file_open.close()
+file_open.close() 
